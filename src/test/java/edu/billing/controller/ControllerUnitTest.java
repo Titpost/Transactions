97027a75fbc58731b61f0831622f600dc4163354
Sergei Titov <Sergei_Titov@epam.com>
2018-02-07 21:47:23 +0300

    cosmetic: got rid of unused

--- src/test/java/edu/billing/controller/ControllerUnitTest.java
+++ src/test/java/edu/billing/controller/ControllerUnitTest.java
@@ -1,6 +1,5 @@
 package edu.billing.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,31 +9,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ControllerUnitTest {
 
     String URL_BASE = "/api/account";
-
     MockMvc mockMvc;
-
-    private String getBaseUrl() {
-        return this.URL_BASE;
-    }
-
 
     /**
      * CORS Headers
      * @throws Exception in MockMvc.perform
      */
     public void test_cors_headers() throws Exception {
-        mockMvc.perform(get(getBaseUrl()))
+        mockMvc.perform(get(URL_BASE))
                 .andExpect(header().string("Access-Control-Allow-Origin", "*"))
                 .andExpect(header().string("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE"))
                 .andExpect(header().string("Access-Control-Allow-Headers", "*"))
                 .andExpect(header().string("Access-Control-Max-Age", "3600"));
-    }
-
-    static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
