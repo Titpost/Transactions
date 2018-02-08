7d2496b8c50c07a9f57c0deaa2d00f2bb0e7adc2
Titov Sergei <quosapv@yandex.ru>
2018-02-09 02:01:06 +0300

    FE added

---
+++ src/main/webapp/front/main.js
@@ -0,0 +1,61 @@
+
+var columnDefs = [
+    {headerName: "id", field: "id", width: 250},
+    {headerName: "amount", field: "amount", width: 90}
+];
+
+var gridOptionsFrom = {
+    columnDefs: columnDefs,
+    rowSelection: 'single',
+    onSelectionChanged: onFromSelectionChanged
+};
+
+var gridOptionsTo = {
+    columnDefs: columnDefs,
+    rowSelection: 'single',
+    onSelectionChanged: onToSelectionChanged
+};
+
+function onFromSelectionChanged() {
+    var selectedRows = gridOptionsFrom.api.getSelectedRows();
+    var selectedRowsString = '';
+    selectedRows.forEach( function(selectedRow, index) {
+        if (index!==0) {
+            selectedRowsString += ', ';
+        }
+    });
+    document.querySelector('#selectedRows').innerHTML = selectedRowsString;
+}
+
+function onToSelectionChanged() {
+    var selectedRows = gridOptionsTo.api.getSelectedRows();
+    var selectedRowsString = '';
+    selectedRows.forEach( function(selectedRow, index) {
+        if (index!==0) {
+            selectedRowsString += ', ';
+        }
+        selectedRowsString += selectedRow.id;
+    });
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function() {
+    var gridDivFrom = document.querySelector('#myGridFrom');
+    new agGrid.Grid(gridDivFrom, gridOptionsFrom);
+
+    var gridDivTo = document.querySelector('#myGridTo');
+    new agGrid.Grid(gridDivTo, gridOptionsTo);
+
+    // do http request to get our sample data - not using any framework to keep the example self contained.
+    // you will probably use a framework like JQuery, Angular or something else to do your HTTP calls.
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'http://localhost:8080/api/account');
+    httpRequest.send();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            var httpResult = JSON.parse(httpRequest.responseText);
+            gridOptionsFrom.api.setRowData(httpResult);
+            gridOptionsTo.api.setRowData(httpResult);
+        }
+    };
+});
