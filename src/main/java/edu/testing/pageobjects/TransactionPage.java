ff5ed4d3c73f0ae1a4e477d694562b0a5721b92b
Titpost <titpost@yandex.ru>
2018-02-12 23:14:56 +0300

    amountLocator

--- src/main/java/edu/testing/pageobjects/TransactionPage.java
+++ src/main/java/edu/testing/pageobjects/TransactionPage.java
@@ -4,11 +4,14 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import edu.testing.base.SelenideBase;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.allure.annotations.Step;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Condition.checked;
@@ -43,8 +46,31 @@ public class TransactionPage {
      */
     @Step
     public void checkSelectedRowsUpdatedAfterTransaction() {
-        gridFrom.stream()
-                .filter( e -> e.getText().equals(SOURCE_ID) )
-                .findFirst();
+
+        final String idLocator = "div[col-id='id']";
+        final String amountLocator = "div[col-id='amount']";
+
+        final Pair<String, String> row = gridFrom.stream()
+                .map(r -> new Pair<>(
+                        r.$(idLocator).getText(),
+                        r.$(amountLocator).getText())
+                )
+                .filter(e -> e.key.equals(SOURCE_ID))
+                .findFirst().get();
+/*
+        final String sourceId = sourceRow
+                .$(idLocator)
+                .getText();
+/*
+        final String sourceAmount = sourceRow
+                .$("div[col-id='id']")
+                .getText();*/
+    }
+
+    @Data
+    @AllArgsConstructor(staticName = "of")
+    public class Pair<F, S> {
+        private F key;
+        private S value;
     }
 }
