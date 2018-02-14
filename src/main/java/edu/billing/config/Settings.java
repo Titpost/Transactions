2aa242b9ef0e4bb474b3f821dc22b3aa8ee275db
Sergei Titov <Sergei_Titov@epam.com>
2018-02-14 15:22:00 +0300

    cosmetic - definitions order

--- src/main/java/edu/billing/config/Settings.java
+++ src/main/java/edu/billing/config/Settings.java
@@ -5,14 +5,13 @@ import java.util.ResourceBundle;
 
 public class Settings {
 
-    private final static String HOST = "host_url";
-    public final static String CONTROLLER_API = "/api/account";
-
     private final static ResourceBundle bundle =
             ResourceBundle.getBundle(
                     "appData",
                     new Locale("en", "EN", "windows")
             );
 
+    private final static String HOST = "host_url";
     public final static String HOST_URL = bundle.getString(HOST);
+    public final static String CONTROLLER_API = "/api/account";
 }
